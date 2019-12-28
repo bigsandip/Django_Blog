31b1cac5054235585257354ada09ef17c8923ef5
bigsandip@gmail.com <bigsandip@gmail.com>
2019-12-28 17:26:48 +0545

    added clas based view and CRUD function

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,62 @@
 from django.shortcuts import render
 from blog.models import Post
 # we can use from .models import Post also
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # from django.http import HttpResponse
-
-# Create your views here.
-
-
-post = [
-    {
-        'title': 'Title1',
-        'author': 'Sandip',
-        'content': 'content1',
-        'date': '17th Dec 2019'
-    },
-    {
-        'title': 'Title2',
-        'author': 'Mishra',
-        'content': 'content2',
-        'date': '20th Dec 2019'
-
-    }
-]
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):  # shortcut then function view
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):  # more shortcut then PostListView
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):  # LoginRequiredMixin is for classed based view and @decorator is for functional view
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):  # to get the id of current loggedin user
+        form.instance.author = self.request.user
+        return super().form_valid(form)  # see models.py after this to set the path to redirect after posting a blog
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):  # to be alble to update he post created by the loggedin users only but not other users post
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):  # more shortcut then PostListView
+    model = Post
+    success_url='/'
+
+    def test_func(self):  # to be alble to update he post created by the loggedin users only but not other users post
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
