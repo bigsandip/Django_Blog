45c2f1936824d02febd1a608cec1b60cae096105
bigsandip@gmail.com <bigsandip@gmail.com>
2019-12-30 21:22:16 +0545

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 # we can use from .models import Post also
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-# from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+# from django.http import HttpResponse
 
 
 # def home(request):
@@ -18,6 +19,18 @@ class PostListView(ListView):  # shortcut then function view
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):  # shortcut then function view
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):  # just to get all the post of username when clicked on their username......
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):  # more shortcut then PostListView
@@ -41,7 +54,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-    def test_func(self):  # to be alble to update he post created by the loggedin users only but not other users post
+    def test_func(self):  # for UserPassesTestMixin to be alble to update he post created by the loggedin users only but not other users post
         post = self.get_object()
         if self.request.user == post.author:
             return True
@@ -50,7 +63,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):  # more shortcut then PostListView
     model = Post
-    success_url='/'
+    success_url = '/'
 
     def test_func(self):  # to be alble to update he post created by the loggedin users only but not other users post
         post = self.get_object()
